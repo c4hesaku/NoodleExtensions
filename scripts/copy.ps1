be7cb823fee467d29ee024fb32ddb77d11efde2c
CatsaCode <catsacode@gmail.com>
2025-10-06 22:07:34 -0400

    Update scripts
    Updating the cmake build files caused issues, but it seems the scripts still work. I use copy.ps1, as it functions in Laurie's template, a lot.

--- scripts/copy.ps1
+++ scripts/copy.ps1
@@ -1,24 +1,81 @@
-param(
-    [Parameter(ValueFromRemainingArguments=$true)]
-    [string[]]$args,
-    [switch]$log = $false,
-    [switch]$release = $false
+Param(
+    [Parameter(Mandatory=$false)]
+    [Switch] $clean,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $log,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $useDebug,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $self,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $all,
+
+    [Parameter(Mandatory=$false)]
+    [String] $custom="",
+
+    [Parameter(Mandatory=$false)]
+    [String] $file="",
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $help
 )
 
-if ($release) {
-    & $PSScriptRoot/build.ps1 -release
-} else {
-    & $PSScriptRoot/build.ps1
+if ($help -eq $true) {
+    Write-Output "`"Copy`" - Builds and copies your mod to your quest, and also starts Beat Saber with optional logging"
+    Write-Output "`n-- Arguments --`n"
+
+    Write-Output "-Clean `t`t Performs a clean build (equvilant to running `"build -clean`")"
+    Write-Output "-UseDebug `t Copies the debug version of the mod to your quest"
+    Write-Output "-Log `t`t Logs Beat Saber using the `"Start-Logging`" command"
+
+    Write-Output "`n-- Logging Arguments --`n"
+
+    & $PSScriptRoot/start-logging.ps1 -help -excludeHeader
+
+    exit
 }
-if ($?) {
-    # adb push build/libchroma.so /sdcard/ModData/com.beatgames.beatsaber/Modloader/mods/libchroma.so
-    if ($?) {
-        adb shell am force-stop com.beatgames.beatsaber
-        adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
-        if ($log.IsPresent) {
-            $timestamp = Get-Date -Format "MM-dd HH:mm:ss.fff"
-            adb logcat -c
-            adb logcat > log.txt
-        }
+
+& $PSScriptRoot/build.ps1 -clean:$clean
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Output "Failed to build, exiting..."
+    exit $LASTEXITCODE
+}
+
+& $PSScriptRoot/validate-modjson.ps1
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
+$modJson = Get-Content "./mod.json" -Raw | ConvertFrom-Json
+
+$modFiles = $modJson.modFiles
+
+foreach ($fileName in $modFiles) {
+    if ($useDebug -eq $true) {
+        & adb push build/debug/$fileName /sdcard/ModData/com.beatgames.beatsaber/Modloader/early_mods/$fileName
+    } else {
+        & adb push build/$fileName /sdcard/ModData/com.beatgames.beatsaber/Modloader/early_mods/$fileName
     }
+}
+
+$lateModFiles = $modJson.lateModFiles
+
+foreach ($fileName in $lateModFiles) {
+    if ($useDebug -eq $true) {
+        & adb push build/debug/$fileName /sdcard/ModData/com.beatgames.beatsaber/Modloader/mods/$fileName
+    } else {
+        & adb push build/$fileName /sdcard/ModData/com.beatgames.beatsaber/Modloader/mods/$fileName
+    }
+}
+
+
+& $PSScriptRoot/restart-game.ps1
+
+if ($log -eq $true) {
+    & adb logcat -c
+    & $PSScriptRoot/start-logging.ps1 -self:$self -all:$all -custom:$custom -file:$file
 }
